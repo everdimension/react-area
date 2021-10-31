ddc05452d3d15d59ada44a921e24847c4916d6b9
everdimension <everdimension@gmail.com>
2021-10-31 13:58:26 +0300

    Use microbundle instead of tsdx

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ export { AreaProvider, RenderArea, Content };
 
 export function createRenderingPair(areaId: string) {
   const pair = {} as { RenderArea: React.FC; Content: React.FC };
-  pair.RenderArea = props => <RenderArea {...props} name={areaId} />;
-  pair.Content = ({ children }) => (
-    <Content name={areaId}>{children}</Content>
-  );
+  pair.RenderArea = (props) => <RenderArea {...props} name={areaId} />;
+  pair.Content = ({ children }) => <Content name={areaId}>{children}</Content>;
   return pair;
 }
